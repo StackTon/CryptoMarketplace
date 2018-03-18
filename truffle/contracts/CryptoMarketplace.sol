4f74bdf20b617ac1e711c6b20a32442a2699e2be
frizeroBG <kovni@abv.bg>
2018-03-18 16:11:05 +0200

    implement CryptoMarketplace

--- truffle/contracts/CryptoMarketplace.sol
+++ truffle/contracts/CryptoMarketplace.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.18;
 
 import "./Ownable.sol";
 import "./SafeMath.sol";
@@ -9,6 +9,10 @@ import "./SafeMath.sol";
 /// @dev store product in mapping of Product
 contract CryptoMarketplace is Ownable {
     using SafeMath for uint;
+    
+    event NewProduct(bytes32 indexed ID, string name, uint price, uint quantity, address owner);
+    event BuyProduct(bytes32 indexed ID, uint quantity, uint value, address buyer);
+    event UpdateProduct(bytes32 indexed ID, uint NewProduct, address owner);
     
     struct Product {
         string name;
@@ -25,21 +29,55 @@ contract CryptoMarketplace is Ownable {
     // hold indexes of all products
     bytes32[] private productIDs;
     
+    modifier onlyValidProduct(bytes32 ID) {
+        // check if the product is greter then zero
+        require(products[ID].price > 0);
+        _;
+    }
+    
     /// @author Alex Stanoev
     /// @notice Buy products by ID
     /// @dev subtraction quantity form products and add it to a soldProducts
     /// @param ID of a product
     /// @param quantity of a product
-    function buy(bytes32 ID, uint quantity) public payable {}
+    function buy(bytes32 ID, uint quantity) public payable onlyValidProduct(ID) {
+        
+        // check if quantity is greter the zero
+        require(quantity > 0);
+        
+        // check if product quantity is greter or equal then quantity param
+        require(products[ID].quantity >= quantity);
+        
+        // inicialize totalPrice variable that with contain how much it cost to buy product
+        uint totalPrice = getPrice(ID, quantity);
+        
+        // check if send wai are greter or equal then totalPrice
+        require(msg.value >= totalPrice);
+        
+        // subtract product quantity by quantity param
+        products[ID].quantity = products[ID].quantity.sub(quantity);
+        
+        // execute BuyProduct event
+        BuyProduct(ID, quantity, totalPrice, msg.sender);
+    }
     
     /// @author Alex Stanoev
     /// @notice update product quantity by ID
     /// @dev check if index is valid and replace the old quantity with the newQuantity
     /// @param ID of a product
     /// @param newQuantity of a product
-    function update(bytes32 ID, uint newQuantity) public {}
+    function update(bytes32 ID, uint newQuantity) public onlyValidProduct(ID) onlyOwner {
+        
+        // check if newQuantity param is different then current product quantity
+        require(newQuantity != products[ID].quantity);
+        
+        // set product quantity to be equal to newQuantity param
+        products[ID].quantity = newQuantity;
+        
+        // execute event UpdateProduct
+        UpdateProduct(ID, newQuantity, msg.sender);
+    }
     
-    //creates a new product and returns its ID
     /// @author Alex Stanoev
     /// @notice add new product
     /// @dev create new product and then push it in the products mapping
@@ -47,7 +85,35 @@ contract CryptoMarketplace is Ownable {
     /// @param price of a product
     /// @param quantity of a product
     /// @return ID of a product
-    function newProduct(string name, uint price, uint quantity) public returns(bytes32) {}
+    function newProduct(string name, uint price, uint quantity) public onlyOwner returns(bytes32) {
+        
+        // check if name param is empty
+        require(bytes(name).length > 0);
+        
+        // check if price is greter then zero
+        require(price > 0);
+        
+        // check if quantity is greter then zero
+        require(quantity > 0);
+        
+        // create newID for the current product
+        bytes32 newID = keccak256(name, ":", price, ":", quantity);
+        
+        // check if there is product with this newID
+        require(products[newID].price == 0);
+        
+        // make and add new product to the products mapping ID => Product
+        products[newID] = Product(name, price, quantity);
+        
+        // push then newID in the productIDs array
+        productIDs.push(newID);
+        
+        
+        // execute event NewProduct
+        NewProduct(newID, name, price, quantity, owner);
+        
+        return newID;
+    }
     
     /// @author Alex Stanoev
     /// @notice get product by ID
@@ -56,13 +122,17 @@ contract CryptoMarketplace is Ownable {
     /// @return name of a product
     /// @return price of a product
     /// @return quantity of a product
-    function getProduct(bytes32 ID) public view returns(string name, uint price, uint quantity) {}
+    function getProduct(bytes32 ID) public view onlyValidProduct(ID) returns(string, uint, uint) {
+        return (products[ID].name, products[ID].price, products[ID].quantity);
+    }
     
     /// @author Alex Stanoev
     /// @notice get all products IDs
     /// @dev -
     /// @return array of bytes32 with all products IDs
-    function getProducts() public view returns(bytes32[]) {}
+    function getProducts() public view returns(bytes32[]) {
+        return productIDs;
+    }
     
     /// @author Alex Stanoev
     /// @notice get product price by ID
@@ -70,10 +140,14 @@ contract CryptoMarketplace is Ownable {
     /// @param ID of a product
     /// @param quantity of a product
     /// @return price of a product
-    function getPrice(bytes32 ID, uint quantity) public view returns (uint) {}
+    function getPrice(bytes32 ID, uint quantity) public view onlyValidProduct(ID) returns (uint) {
+        return products[ID].price.mul(quantity);
+    }
     
     /// @author Alex Stanoev
     /// @notice withdrawal all contract money to owner
     /// @dev -
-    function withdrawalMoney() public {}
+    function withdrawalMoney() public onlyOwner {
+        owner.transfer(this.balance);
+    }
 }
