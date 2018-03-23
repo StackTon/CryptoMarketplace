e6c8a4960d10fab816e70bcd015177a3afad1ec8
frizeroBG <kovni@abv.bg>
2018-03-23 22:54:12 +0200

    implement getWeb3 func and getSubjects func

--- react/src/components/ProductsPage/AllProducts.jsx
+++ react/src/components/ProductsPage/AllProducts.jsx
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
+import { getWeb3, contractABI, contractAddress } from "../../api/remote"
 
 export default class AllProductsPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            web3: null
+        }
+
+        this.getProducts = this.getProducts.bind(this);
+    }
+
+    componentDidMount() {
+        getWeb3.then(results => {
+            this.setState({
+                web3: results.web3
+            })
+            this.getProducts();
+        }).catch((err) => {
+            console.log(err);
+            console.log('Error finding web3.')
+        })
+    }
+
+    getProducts() {
+        const cryotoCharityInstance = this.state.web3.eth.contract(contractABI).at(contractAddress);
+
+        cryotoCharityInstance.getProducts.call((err, res) => {
+            if(err){
+                console.log(err);
+                return;
+            }
+
+            console.log(res);
+        })
+
+    }
+
     render() {
         return (
             <div className="container">
